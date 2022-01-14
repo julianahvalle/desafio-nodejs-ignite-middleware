21d64e0184b3e7136251562a3c28e2938c43092e
Juliana Valle <mfrasao@hotmail.com>
2022-01-14 17:26:16 -0300

    aplicação passou em todos os testes de middlerware

--- src/index.js
+++ src/index.js
@@ -10,19 +10,66 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(user => user.username === username);
+
+  if(!user){
+    return response.status(404).json({error:'User not found'});
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  if(user.pro === false && user.todos.length >= 10){
+    return response.status(403).json({error:'You don\'t have permission to do this action'});
+  }
+
+  return next();
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  if(!validate(id)){
+    return response.status(400).json({error:'Invalid id'})
+  }
+  
+  const user = users.find((user) => user.username === username);
+  if(!user){
+    return response.status(404).json({error:'User not found'});
+  }
+  
+  const todo = user.todos.find((todo) => todo.id === id);
+
+  if(!todo){
+    return response.status(404).json({error: 'To do not found'})
+  }
+
+  request.user = user;
+  request.todo = todo;
+
+  return next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find(user => user.id === id);
+  if(!user){
+    return response.status(404).json({error:'User id not foud'})
+  }
+  
+  request.user = user;
+  
+  return next();
+  
 }
 
 app.post('/users', (request, response) => {
